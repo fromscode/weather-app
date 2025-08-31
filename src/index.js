d9ef0e26bb52878a934fbc1d35efee55dbd9ab28
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-08-31 05:42:58 +0000

    Add main and side weather doms

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles/reset.css";
 import "./styles/variables.css";
 import "./styles/styles.css";
+import "./styles/mainWeather.css";
+import "./styles/sideWeather.css";
 
 import getWeatherJson from "./styles/getWeather";
 import processJson from "./processJson";
@@ -12,6 +14,42 @@ processForm(renderWeather);
 
 async function renderWeather(city) {
   const obj = await processJson(getWeatherJson(city));
+  console.log(obj);
+
+  renderMainWeather(obj);
+  renderSideWeather(obj);
+}
+
+function renderMainWeather(obj) {
   const mainWeather = document.querySelector(".main-weather");
-  mainWeather.textContent = obj.days[0].temp;
+  const temp = mainWeather.querySelector(".temp");
+  temp.textContent = obj.days[0].temp;
+
+  const max = mainWeather.querySelector(".max");
+  max.textContent = obj.days[0].tempmax;
+
+  const min = mainWeather.querySelector(".min");
+  min.textContent = obj.days[0].tempmin;
+
+  const icon = mainWeather.querySelector(".icon");
+  icon.textContent = obj.days[0].icon;
+
+  const feelsLike = mainWeather.querySelector(".feels-like");
+  feelsLike.textContent = obj.days[0].feelslike;
+}
+
+function renderSideWeather(obj) {
+  const sideWeather = document.querySelector(".side-weather");
+
+  const humidity = sideWeather.querySelector(".humidity");
+  humidity.textContent = obj.days[0].humidity;
+
+  const precipitation = sideWeather.querySelector(".precipitation");
+  precipitation.textContent = obj.days[0].precipprob;
+
+  const windSpeed = sideWeather.querySelector(".windspeed");
+  windSpeed.textContent = obj.days[0].windspeed;
+
+  const uv = sideWeather.querySelector(".uv-index");
+  uv.textContent = obj.days[0].uvindex;
 }
--- src/styles/getWeather.js
+++ src/styles/getWeather.js
@@ -1,7 +1,7 @@
 export default async function getWeatherJson(city = "Rome") {
   try {
     const respose = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Cname%2Clatitude%2Clongitude%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Cdew%2Chumidity%2Cprecipprob%2Csnow%2Cwindspeed%2Cpressure%2Cuvindex&include=days%2Chours&key=QG7KVPYMWLNGYA7R6NLCECND7&contentType=json`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Chumidity%2Cprecipprob%2Cwindspeed%2Cpressure%2Cvisibility%2Cuvindex%2Cconditions%2Cicon&include=days%2Chours&key=QG7KVPYMWLNGYA7R6NLCECND7&contentType=json`,
     );
     return await respose.json();
   } catch (err) {
